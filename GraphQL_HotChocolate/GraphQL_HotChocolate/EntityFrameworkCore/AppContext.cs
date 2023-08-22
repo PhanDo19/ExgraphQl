141713540acb8182ebb58c75e7dda34c16b08dd8
PhanDo19 <94103740+PhanDo19@users.noreply.github.com>
2023-08-22 16:00:17 +0700

    Create dbcontext

---
+++ GraphQL_HotChocolate/GraphQL_HotChocolate/EntityFrameworkCore/AppContext.cs
@@ -0,0 +1,19 @@
+﻿using GraphQL_HotChocolate.Extention;
+using GraphQL_HotChocolate.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQL_HotChocolate.EntityFrameworkCore
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext( DbContextOptions<AppDBContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           modelBuilder.SeedProduct();
+        }
+    }
+}
---
+++ GraphQL_HotChocolate/GraphQL_HotChocolate/Extention/SeedData.cs
@@ -0,0 +1,19 @@
+﻿using GraphQL_HotChocolate.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQL_HotChocolate.Extention
+{
+    public static class SeedData
+    {
+        public static void SeedProduct(this ModelBuilder modelBuilder)
+        {
+            var newList = new List<Product>() {
+                    new Product{Id= 1, Name="Hoa",Category="Hoa",Description ="Hoa de ban"},
+                    new Product{Id= 2, Name="Banh",Category="Banh",Description ="Hoa de ban"},
+                    new Product{Id= 3, Name="Keo",Category="keo",Description ="Hoa de ban"}
+                };
+            modelBuilder.Entity<Product>().HasData(newList);
+
+        }
+    }
+}
